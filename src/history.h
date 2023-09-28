587a53df42d0dd37b3127d81f51222e8bcbb8fc5
toastloop <69254179+toastloop@users.noreply.github.com>
2023-09-28 14:36:57 -0600

    Moved item and list definition into it's own header file

--- src/history.h
+++ src/history.h
@@ -1,15 +1,7 @@
 #ifndef _HISTORY_
 #define _HISTORY_
 
-typedef struct s_Item {
-  int id;
-  char *str;
-  struct s_Item *next;
-} Item;
-
-typedef struct s_List {
-  struct s_Item *root;
-} List;
+#include "list.h"
 
 /* Initialize the linked list to keep the history. */
 List* init_history();
---
+++ src/list.h
@@ -0,0 +1,14 @@
+#ifndef __LIST__
+#define __LIST__
+
+typedef struct s_Item {
+  int id;
+  char *str;
+  struct s_Item *next;
+} Item;
+
+typedef struct s_List {
+  struct s_Item *root;
+} List;
+
+#endif
