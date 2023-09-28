00f80d9617cb7803a4acb7260f2025d9006a2c3e
toastloop <69254179+toastloop@users.noreply.github.com>
2023-09-28 14:39:16 -0600

    Created uimain to handle main interface and user input

---
+++ src/uimain.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "tokenizer.h"
+#include "history.h"
+#include "list.h"
+#include "util.h"
+
+int main(int argc, char **argv)
+{
+    ((void)argc, (void)argv);
+    char *line = malloc(sizeof(char) * 1000);
+    List *history = init_history();
+
+    while (1)
+    {
+        fputs(">>> ", stdout);
+        fgets(line, 1000, stdin);
+        fseek(stdin,0,SEEK_END);
+        fseek(stdout,0,SEEK_END);
+        if(strcmp(line, "\n") == 0)
+        {
+            break;
+        }
+        if(line[strlen(line) - 1] == '\n')
+        {
+            line[strlen(line) - 1] = '\0';
+        }
+        if(*line == '!')
+        {
+            char *str = get_history(history, chartoint(line + 1));
+            if(str != 0)
+            {
+                printf("%s\n", str);
+            }
+            else
+            {
+                printf("No command found\n");
+            }
+        }
+        else if(strcmp(line, "history") == 0)
+        {
+            print_history(history);
+        }
+        else if(strcmp(line, "exit") == 0)
+        {
+            break;
+        }
+        else
+        {
+            add_history(history, line);
+            char **tokens = tokenize(line);
+            print_tokens(tokens);
+            free_tokens(tokens);
+        }
+    }
+    free_history(history);
+    return 0;
+}
+
